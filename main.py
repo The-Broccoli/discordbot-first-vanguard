2dcf38b37a5ea894af632fafe9d25002fde07850
The-Broccoli <The-Broccoli@users.noreply.github.com>
2021-12-11 18:27:03 +0100

    add main.py and config.ini structure

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+from configparser import ConfigParser
+
+import discord
+from discord.ext import commands
+
+
+def main():
+
+    # Load config
+    file = 'config.ini'
+    config = ConfigParser()
+    config.read(file)
+
+    intents = discord.Intents.all()
+    client = commands.Bot(command_prefix=">", intents=intents)
+
+    # Ready message
+    @client.event
+    async def on_ready():
+        print('We have logged in as {0.user}'.format(client))
+
+    # Load cogs
+    for folder in os.listdir("modules"):
+        if os.path.exists(os.path.join('modules', folder, 'cog.py')):
+            client.load_extension(f'modules.{folder}.cog')
+    
+    client.run(config.get('bot_info', 'token'))
+
+if __name__ == '__main__':
+    main()
