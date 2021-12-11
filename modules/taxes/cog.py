8eb7071d7e4704c2ba89c432d1d5599831118a62
The-Broccoli <The-Broccoli@users.noreply.github.com>
2021-12-11 23:45:09 +0100

    add info embed in taxes command
    unfortunately only with demo values

--- modules/taxes/cog.py
+++ modules/taxes/cog.py
@@ -1,4 +1,5 @@
 from configparser import ConfigParser
+from datetime import datetime, timedelta
 
 import discord
 from discord.ext import commands
@@ -19,11 +20,29 @@ class Taxes(commands.Cog, name="Taxes"):
     @commands.command()
     async def taxes(self, ctx: commands.Context):
         """TODO Taxes command"""
+
+        intervalValue = int(self.config.get('dc_taxes_commands', 'interval_value'))
+        unpaidTaxes = 45 # TODO DEMO VALUE
+        paidTaxes = 23 # TODO DEMO VALUE
+        blacklisted = 5 # TODO DEMO VALUE
+        presenceTime = [datetime.today() + (timedelta(hours=0, minutes=(unpaidTaxes * intervalValue)) - timedelta(hours=0, minutes=(blacklisted * intervalValue))),
+                        (unpaidTaxes * intervalValue) - (blacklisted * intervalValue)]
+        presenceTime[0] = presenceTime[0].strftime('%H:%M - (%m/%d/%Y)')
+
         try:
             for i in ctx.author.roles:
                 if i.id == int(self.config['role']['bot_commander']):
                     """With this command you can start the tax notification"""
-                    pass # TODO Sequence of the command
+                    embedVar01 = discord.Embed(title='Gildensteuer Benachrichtigung',
+                                            description=f'Hallo {ctx.message.author.mention}, mit diesem Befehl kannst du eine Private Notification **an alle Mitglieder** automatisiert versenden, die noch keine Steuern in dieser KW gezahlt haben (aufgenommen Mitglieder, die diesen Dienst verweigert haben).',
+                                            color=discord.Colour.green())
+                    embedVar01.add_field(name="Ablauf",
+                                        value=f'Sobald du auf "Absenden" klickst, werde die Nachrichten in einem Intervall von {intervalValue} Minuten versendet. Zwischen dem Versenden der Nachrichten ist so genügen Zeit, um die Beiträge einzutragen.\n`({unpaidTaxes} (Unbezahlt) - 5 (Blacklist)) * {intervalValue} Minuten = {presenceTime[1]}`\n Anwesenheit gewähren bis **{presenceTime[0]}**',
+                                        inline=True)
+                    embedVar01.add_field(name='Tax status',
+                                        value=f'Offen: **{unpaidTaxes}**\tBezahlt: **{paidTaxes}**',
+                                        inline=True)
+                    await ctx.send(embed=embedVar01)
             # await ctx.message.delete() # Delete message, does this make sense ?
         except Exception as e:
             errorEmbed = discord.Embed(title='Something went wrong!',
