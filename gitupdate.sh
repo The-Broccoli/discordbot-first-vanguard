c474417a21ab8538a8994764f8b8dd91b7168d3c
The-Broccoli <The-Broccoli@users.noreply.github.com>
2021-12-15 14:55:48 +0100

    Update gitupdate.sh

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #Alle 15 Minuten per Cronjob
-#*/15 * * * * cd masterserver/forgottennw-discordbot/ && timeout 300 ./gitupdate.sh >/dev/null 2>&1
+#*/15 * * * * cd masterserver/forgottennw-discord/ && timeout 300 ./gitupdate.sh >/dev/null 2>&1
 
 git pull
